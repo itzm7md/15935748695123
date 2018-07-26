f4fbd77e2ed3960e40b52619d5877be32acb96e5
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-07-26 23:34:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -200,66 +200,24 @@ if (msg.content === 'بفجرابوك') {
       msg.reply('**كل زق**');
     }
   });
-const fs = require('fs')
-const Canvas = require('canvas')
-const jimp = require('jimp')
-const moment = require('moment')
-const prefix = '/';
-
-client.on('message', message => {
-    
-    let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-    let guild = (client, message).guild;
-     let user = message.mentions.users.first();
-     var men = message.mentions.users.first();
-        var heg;
-        if(men) {
-            heg = men
-        } else {
-            heg = message.author
-        }
-      var mentionned = message.mentions.members.first();
-         var h;
-        if(mentionned) {
-            h = mentionned
-        } else {
-            h = message.member
-        }
-       moment.locale('ar-TN');
-{
-  let args = message.content.split(" ").slice(1);
-if(command == "id") {
-    var Canvas = require('canvas')
-  , Image = new Canvas.Image
-  , canvas = new Canvas(450, 170)
-  , ctx = canvas.getContext('2d');
-  ctx.font = '15px Impact';
-  
-Image.src = canvas.toBuffer();
-
-
-    
-ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
-ctx.fillText(`${moment(heg.createdTimestamp).fromNow()}`,90, 20);
-ctx.fillText(`${moment(h.joinedAt).fromNow()}`,110, 70);
-ctx.fillText(`${heg.username}`,70, 40);
-ctx.fillText(`${h.presence.status}`,50, 70);
-
-
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.stroke();
-
-
-
-message.channel.sendFile(canvas.toBuffer());
-}
+client.on("message", msg => {
+    var prefix = "/";
+if(msg.content.startsWith (prefix + "id")) {
+if(!msg.channel.guild) return msg.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
+const embed = new Discord.RichEmbed();
+embed.addField(":cloud_tornado:  الاسم", `**[ ${msg.author.username}#${msg.author.discriminator} ]**`, true)
+   .addField(":id:  الايدي", `**[ ${msg.author.id} ]**`, true)
+   .setColor("RANDOM")
+   .setFooter(msg.author.username , msg.author.avatarURL)
+   .setThumbnail(`${msg.author.avatarURL}`)
+   .setTimestamp()
+   .setURL(`${msg.author.avatarURL}`)
+   .addField(':spy:  الحالة', `**[ ${msg.author.presence.status.toUpperCase()} ]**`, true)
+   .addField(':satellite_orbital:   يلعب', `**[ ${msg.author.presence.game === null ? "No Game" : msg.author.presence.game.name} ]**`, true)
+   .addField(':military_medal:  الرتب', `**[ ${msg.member.roles.filter(r => r.name).size} ]**`, true)
+   .addField(':robot:  هل هو بوت', `**[ ${msg.author.bot.toString().toUpperCase()} ]**`, true);
+msg.channel.send({embed: embed})
 }
 });
-
-
 client.login("NDcyMTQzNzQxNTY2NzEzODY3.DjvHQA.R8PmQeo3yBV4HNLmz86KjjHHfPk");
 //CODES ReBeL
