e062d2a6ce4f8d088d9935ee351bef9969f3a99b
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-07-26 23:39:56 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,5 +219,56 @@ embed.addField(":cloud_tornado:  الاسم", `**[ ${msg.author.username}#${msg.
 msg.channel.send({embed: embed})
 }
 });
+client.on('message', message => {
+    if(message.content == / + 'slist') {
+             if(!message.author.id === '323885452207587329') return;
+             if(!message.author.id === '334435543851204618') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    let serv = new Discord.RichEmbed()
+    .setAuthor(gname,gimg)
+    .setThumbnail(gimg)
+    .addField('Server Member Count',gmemb = g.members.size)
+    .setColor('RANDOM')
+    message.channel.send(`
+    
+            `);
+          message.channel.sendEmbed(serv);
+    }) 
+    }
+    });
+    
+    client.on('message', message => {
+    if(message.content == / + 'slis') {
+             if(!message.author.id === '323885452207587329') return;
+             if(!message.author.id === '334435543851204618') return;
+    var gimg;
+    var gname;
+    var gmemb;
+    var gbots;
+    var groles;
+    var servers = client.guilds;
+    servers.forEach((g)=>{
+    gname = g.name;
+    gimg = g.iconURL;
+    gmemb = g.members.size;
+    let serv = new Discord.RichEmbed()
+    message.channel.send(`
+    **-------------------------**
+      Server Name : **${gname}**
+      Server MemberCount : **${gmemb} **
+      **---------------------------**
+            `);
+    }) 
+    }
+    });
 client.login("NDcyMTQzNzQxNTY2NzEzODY3.DjvHQA.R8PmQeo3yBV4HNLmz86KjjHHfPk");
 //CODES ReBeL
