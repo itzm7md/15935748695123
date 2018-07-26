b6e92999432331a8b94696831404154de9c31bf0
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-07-26 23:49:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -219,57 +219,7 @@ embed.addField(":cloud_tornado:  الاسم", `**[ ${msg.author.username}#${msg.
 msg.channel.send({embed: embed})
 }
 });
-client.on('message', message => {
-    if(message.content == / + 'slist') {
-             if(!message.author.id === '323885452207587329') return;
-             if(!message.author.id === '334435543851204618') return;
-    var gimg;
-    var gname;
-    var gmemb;
-    var gbots;
-    var groles;
-    var servers = client.guilds;
-    servers.forEach((g)=>{
-    gname = g.name;
-    gimg = g.iconURL;
-    gmemb = g.members.size;
-    let serv = new Discord.RichEmbed()
-    .setAuthor(gname,gimg)
-    .setThumbnail(gimg)
-    .addField('Server Member Count',gmemb = g.members.size)
-    .setColor('RANDOM')
-    message.channel.send(`
-    
-            `);
-          message.channel.sendEmbed(serv);
-    }) 
-    }
-    });
-    
-    client.on('message', message => {
-    if(message.content == / + 'slis') {
-             if(!message.author.id === '323885452207587329') return;
-             if(!message.author.id === '334435543851204618') return;
-    var gimg;
-    var gname;
-    var gmemb;
-    var gbots;
-    var groles;
-    var servers = client.guilds;
-    servers.forEach((g)=>{
-    gname = g.name;
-    gimg = g.iconURL;
-    gmemb = g.members.size;
-    let serv = new Discord.RichEmbed()
-    message.channel.send(`
-    **-------------------------**
-      Server Name : **${gname}**
-      Server MemberCount : **${gmemb} **
-      **---------------------------**
-            `);
-    }) 
-    }
-    });
+
 client.on("message", (message) => {
     if (message.content.startsWith("/cv")) {
                 if (!message.member.hasPermission('MANAGE_CHANNELS')) return message.reply("You Don't Have `MANAGE_CHANNELS` Premissions ");
